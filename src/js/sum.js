f1625a82a9511b079b6ea3e5f0e6902634fa0ce6
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-03-01 14:27:30 +0200

    no full

---
+++ src/js/sum.js
@@ -0,0 +1,6 @@
+
+
+export const countSum = (fistNumber, secondNumber) => 
+    Number.parseFloat(firstNumInput.value) - Number.parseFloat(secondNumInput.value);
+
+
---
+++ src/main.js
@@ -0,0 +1,1 @@
+import { countSum } from "./js/sum"
